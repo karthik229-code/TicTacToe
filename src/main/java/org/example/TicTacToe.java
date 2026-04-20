abd3791dce038e5d7fec43dde3df85a4031afade
karthikghnana <karhikghnana@gmail.com>
2026-04-20 13:35:38 +0530

    initialized board

--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -1,7 +1,34 @@
-public class TicTacToe{
+public class TicTacToe {
+
+    static char[][] board = new char[3][3];
+
     public static void main(String[] args) {
-       System.out.println("TicTacToe Game");
+        initializeBoard();
+        printBoard();
+    }
+
+    // Initialize the board with '-'
+    static void initializeBoard() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                board[row][col] = '-';
+            }
+        }
+    }
+
+    // Print the board in grid format
+    static void printBoard() {
+        System.out.println("-------------");
+
+        for (int row = 0; row < 3; row++) {
+            System.out.print("| ");
+
+            for (int col = 0; col < 3; col++) {
+                System.out.print(board[row][col] + " | ");
+            }
+
+            System.out.println();
+            System.out.println("-------------");
+        }
     }
 }
-
-
